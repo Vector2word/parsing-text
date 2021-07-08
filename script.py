b0db4343d348d98ef1433cad1ddf7aa4033b6439
Olga P <85909010+Vector2word@users.noreply.github.com>
2021-07-07 22:46:50 -0400

    Add files via upload

---
+++ script.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 21:14:45 2021
+
+@author: My Lenovo
+"""
+
+import spacy
+from nltk import Tree
+from squids import squids_text
+
+
+
+dependency_parser = spacy.load('en_core_web_sm')
+
+parsed_squids = dependency_parser(squids_text)
+
+# Assign my_sentence a new value:
+my_sentence = "How does spaCy know how to parse this sentence?"
+my_parsed_sentence = dependency_parser(my_sentence)
+
+def to_nltk_tree(node):
+  if node.n_lefts + node.n_rights > 0:
+    parsed_child_nodes = [to_nltk_tree(child) for child in node.children]
+    return Tree(node.orth_, parsed_child_nodes)
+  else:
+    return node.orth_
+
+for sent in parsed_squids.sents:
+  to_nltk_tree(sent.root).pretty_print()
+  
+for sent in my_parsed_sentence.sents:
+ to_nltk_tree(sent.root).pretty_print()
---
+++ squids.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 21:15:31 2021
+
+@author: My Lenovo
+"""
+
+squids_text = "So many squids are jumping out of suitcases these days. You can barely go anywhere without seeing one. I went to the dentist the other day. Sure enough, I saw an angry one jump out of my dentist's bag. She hardly even noticed."
